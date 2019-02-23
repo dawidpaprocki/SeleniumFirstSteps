db483c65bc45852d0631d6753bb5e7c3a5fe1d1a
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-02-23 13:41:37 +0100

    DynamircControlsTests added
     LoadingBar test

---
+++ src/test/java/pages/DynamicControls.java
@@ -0,0 +1,35 @@
+package pages;
+
+import lombok.Data;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Data
+public class DynamicControls extends BasePage{
+
+    @FindBy(css = "input[type='checkbox']")
+    private WebElement checkBox;
+    @FindBy(css = "button[type='button']")
+    private List<WebElement> listOFButton;
+    @FindBy(id = "loading")
+    private WebElement loadingBar;
+    @FindBy(id = "message")
+    private WebElement message;
+
+    public DynamicControls() {
+        super();
+    }
+    public Boolean visibilityOfLoadingBar(){
+       String style = loadingBar.getCssValue("style");
+        return style.equals("");
+    }
+    public WebElement getButton(String button){
+        Optional<WebElement> webElement1 = listOFButton.stream().findFirst().filter(webElement -> webElement.getText().equals(button));
+        return webElement1.get();
+    }
+    
+}
---
+++ src/test/java/tests/DynamicControlsTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import config.TestConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import pages.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DynamicControlsTest extends TestConfig {
+    private Home home;
+
+    @Before
+    public void Before() {
+        home = new Home();
+    }
+
+
+    @Test
+    public void TestDynamicControlsLoadingBarVisibility() throws InterruptedException {
+        //given
+        home.getDynamicControls().click();
+        DynamicControls dynamicControls = new DynamicControls();
+        //when
+        dynamicControls.getButton("Remove").click();
+        Thread.sleep(2000);
+        Boolean visibilityOfElement = dynamicControls.visibilityOfLoadingBar();
+        //then
+        assertTrue(visibilityOfElement);
+    }
+
+
+
+}
